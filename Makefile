50786568e0b0e868694aed4b3cebb93d792445cb
Sean <seanmoore1976@gmail.com>
2023-01-16 12:32:12 -0800

    Code updates per change requests for Quickstart guide redesign
    Addressing change requests to code example per the pull request related to the Quickstart guide redesign.
    
    https://github.com/streamingfast/substreams/pull/141

--- Makefile
+++ Makefile
@@ -4,9 +4,9 @@ build:
 
 .PHONY: stream
 stream: build
-	substreams run -e mainnet.eth.streamingfast.io:443 substreams-ethereum-tutorial.yaml map_basic_eth -s 12292922 -t +10
+	substreams run -e mainnet.eth.streamingfast.io:443 substreams.yaml map_basic_eth -s 12292922 -t +10
 
 .PHONY: codegen
 codegen:
-	substreams protogen ./substreams-ethereum-tutorial.yaml --exclude-paths="sf/substreams,google"
+	substreams protogen ./substreams.yaml --exclude-paths="sf/substreams,google"
 	
--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,18 @@ mod pb;
 
 use pb::basicexample;
 
-use substreams::{log};
+use substreams::{log, Hex};
 use substreams_ethereum::{pb as ethpb};
 
 #[substreams::handlers::map]
 fn map_basic_eth(block: ethpb::eth::v2::Block) -> Result<basicexample::BasicExampleProtoData, substreams::errors::Error> {
-    // Extract data from the Ethereum Block and log to the console.
-    // The data available in the Block directly represents the related protobuf.
-    // The full data model for an Ethereum Block is available at the following link.
-    // https://github.com/streamingfast/firehose-ethereum/blob/develop/proto/sf/ethereum/type/v2/type.proto
-    log::info!("block.ver: {:#?}", block.ver);
+    
+    let header = block.header.as_ref().unwrap();
+    
     log::info!("block.number: {:#?}", block.number);
-
-    // Copy the data in the Block's version field and return it to caller.
-    // Substreams developers will typically pass extracted data through a custom
-    // protobuf to a store module.
-    Ok(basicexample::BasicExampleProtoData {version: block.ver})
+    log::info!("block.hash: {:#?}",  Hex(&block.hash).to_string());
+    log::info!("block.header.parent_hash: {:#?}", Hex(&header.parent_hash).to_string());
+    log::info!("block.header.timestamp: {:#?}", header.timestamp.as_ref().unwrap().to_string());
+    
+    Ok(basicexample::BasicExampleProtoData {number: block.number, hash: Hex(&block.hash).to_string(), parent_hash: Hex(&header.parent_hash).to_string(), timestamp: header.timestamp.as_ref().unwrap().to_string()})
 }
